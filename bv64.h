db10ec92c00455b89bd7468335fd453180cf599b
Darrell Long <darrell@pentexoire.com>
2021-02-02 14:41:46 -0800

    fix

--- bv64.h
+++ bv64.h
@@ -12,6 +12,10 @@ typedef struct bitV {
   uint32_t l;
 } bitV;
 
+static inline uint32_t index(uint32_t i) {
+    return !i || i % BITS_PER_UNIT ? i / BITS_PER_UNIT : i / BITS_PER_UNIT - 1;
+}
+
 static inline bitV *newVec(uint32_t l) {
   bitV *v = (bitV *) malloc(sizeof(bitV));
   if (v) {
@@ -36,20 +40,20 @@ static inline void delVec(bitV *v) {
 
 static inline void setBit(bitV *x, uint32_t k) {
   if (x && x->v) {
-    x->v[k / BITS_PER_UNIT] |= ((uint64_t) 0x1 << k % BITS_PER_UNIT);
+    x->v[index(k)] |= ((uint64_t) 0x1 << k % BITS_PER_UNIT);
   }
   return;
 }
 
 static inline void clrBit(bitV *x, uint32_t k) {
   if (x && x->v) {
-    x->v[k / BITS_PER_UNIT] &= ~((uint64_t) 0x1 << k % BITS_PER_UNIT);
+    x->v[index(k)] &= ~((uint64_t) 0x1 << k % BITS_PER_UNIT);
   }
   return;
 }
 
 static inline bool valBit(bitV *x, uint32_t k) { // We must assume a valid pointer
-  return (x->v[k / BITS_PER_UNIT] >> k % BITS_PER_UNIT) & 0x1;
+  return (x->v[index(k)] >> k % BITS_PER_UNIT) & 0x1;
 }
 
 static inline uint32_t lenVec(bitV *x) {
